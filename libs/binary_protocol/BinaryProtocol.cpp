32d8cec4ab2f5d7af112eb063a97d2bb52d92d86
Clément Lagier <clement.lagier@epitech.eu>
2024-10-15 07:31:09 +0400

    [ADD] Added a BinaryProtocol class

---
+++ libs/binary_protocol/BinaryProtocol.cpp
@@ -0,0 +1,11 @@
+#include "BinaryProtocol.hpp"
+
+johLibs::BinaryProtocol::BinaryProtocol()
+{
+  std::cout << "BinaryProtocol created" << std::endl;
+}
+
+johLibs::BinaryProtocol::~BinaryProtocol()
+{
+  std::cout << "BinaryProtocol destroyed" << std::endl;
+}
---
+++ libs/binary_protocol/BinaryProtocol.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+namespace johLibs {
+  class BinaryProtocol {
+    public:
+      BinaryProtocol();
+      ~BinaryProtocol();
+
+  };
+}
