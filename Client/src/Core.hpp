b2640240abcdc2806433e405c275dc8efbd56b03
Antonin Laudon <antonin.laudon@epitech.eu>
2024-11-06 10:49:40 +0100

    Add: Main core class.

---
+++ Client/src/Core.hpp
@@ -0,0 +1,45 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <regex>
+
+#include "Enum.hpp"
+
+class Core
+{
+    public:
+        Core();
+        Core(sf::Vector2<int> windowSize, sf::Vector2<int> gameSize, std::pair<std::string, int> ipPort);
+        ~Core();
+
+        void setWindowSize(sf::Vector2<int> windowSize);
+        void setGameSize(sf::Vector2<int> gameSize);
+        int setIpPort(std::pair<std::string, unsigned short> ipPort);
+
+        void run();
+    
+    private:
+        void _draw(); // Not defined yet
+        void _update(); // Not defined yet
+        void _setSnake(int id, std::vector<std::pair<int, int>> snakePos); // Not defined yet
+        void _setApple(std::pair<int, int> applePos); // Not defined yet
+
+        sf::TcpSocket _socket;
+        sf::RenderWindow _window;
+        sf::Event _event;
+        sf::Vector2<int> _windowSize;
+        sf::Vector2<int> _gameSize;
+        std::vector<std::vector<sf::RectangleShape>> _snake;
+        sf::RectangleShape _apple;
+};
+
+#endif // CORE_HPP
--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,9 +1,54 @@
-#include <iostream>
-#include "Graphics.hpp"
+#include "Core.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+std::pair<std::string, unsigned short> parseArguments(int argc, char **argv) {
+    // Check if the user wants to see the help message
+    if (argc == 2 && (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help")) {
+    std::cout << "Usages:\n"
+              << "  " << argv[0] << " -h | --help\n"
+              << "  " << argv[0] << " <ip> <port>\n\n"
+              << "Arguments:\n"
+              << "  -h, --help    Show this help message and exit\n"
+              << "  <ip>          Specify the IP address to connect to\n"
+              << "  <port>        Specify the port number to use for the connection\n\n"
+              << "Example:\n"
+              << "  " << argv[0] << " 192.168.1.1 8080\n";
+        return std::make_pair("__EXIT__", 0);
+    }
 
-    Graphics graphics(sf::Vector2<int>(800, 600), sf::Vector2<int>(800, 600));
+    // Check if the user provided the correct number of arguments
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " -h" << std::endl;
+        return std::make_pair("__EXIT__", 1);
+    }
+
+    // Check if the IP address is valid
+    std::regex ipRegex("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$");
+
+    if (!std::regex_match(argv[1], ipRegex)) {
+        std::cerr << "Invalid ip address, expected format: x.x.x.x with x in [0, 255]" << std::endl;
+        return std::make_pair("__EXIT__", 1);
+    }
+
+    // Check if the port number is valid
+    try {
+        int port = std::stoi(argv[2]);
+        if (port < 0 || port > 65535) throw std::invalid_argument("Invalid port");
+    } catch (std::invalid_argument &e) {
+        std::cerr << "Invalid port, expected a number between 0 and 65535" << std::endl;
+        return std::make_pair("__EXIT__", 1);
+    }
+
+    return std::make_pair(argv[1], std::stoi(argv[2]));
+}
+
+int main(int argc, char **argv) {
+    std::pair<std::string, unsigned short> ipPort = parseArguments(argc, argv);
+    Core core;
+
+    if (ipPort.first == "__EXIT__") return ipPort.second;
+    if (core.setIpPort(ipPort)) return 1; // Failed to connect to the server
+    core.setGameSize(sf::Vector2<int>(20, 20));
+    core.setWindowSize(sf::Vector2<int>(800, 800));
+    core.run();
     return 0;
 }
